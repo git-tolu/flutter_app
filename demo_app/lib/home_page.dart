e904ae42a9f7e6fa29ad34e0e4ffe058c3a35390
git-tolu <adejumobitoluwanimi2@gmail.com>
2023-01-08 22:40:16 +0100

    flutter second page and multiple widget commit

---
+++ demo_app/lib/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:demo_app/learn_flutter_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (BuildContext context) {
+                return const LearnFlutterPage();
+              },
+            ),
+          );
+        },
+        child: const Text('Learn Flutter'),
+      ),
+    );
+  }
+}
---
+++ demo_app/lib/learn_flutter_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({super.key});
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Learn Flutter'),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Icon(Icons.arrow_back)),
+      ),
+      body: Column(
+        children: [
+          Image.asset('images/36928.jpg'),
+          const Divider(
+            color: Colors.black,
+          ),
+        ],
+      ),
+    );
+  }
+}
